9eae3d4655b034f9c96998f6e0cede0df33b47b5
RafaelRodrigues44 <rafael.rodrigues85@hotmail.com>
2026-02-08 05:11:51 -0300

    Replace Readmes

--- etl/database.py
+++ etl/database.py
@@ -1,9 +1,16 @@
 import os
+import streamlit as st 
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-load_dotenv()
-DB_URL = os.getenv("DATABASE_URL")
+def get_db_url():
+    try:
+        return st.secrets["DATABASE_URL"]
+    except (FileNotFoundError, KeyError):
+        load_dotenv()
+        return os.getenv("DATABASE_URL")
+
+DB_URL = get_db_url()
 
 def get_db_engine():
     if not DB_URL:
