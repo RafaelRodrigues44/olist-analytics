93f2a1f4f413dedb695cf1291c884ebf3ecee633
RafaelRodrigues44 <145143991+RafaelRodrigues44@users.noreply.github.com>
2026-02-08 05:24:23 -0300

    Update database.py

--- etl/database.py
+++ etl/database.py
@@ -3,11 +3,13 @@ import streamlit as st
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
+load_dotenv()
+
 def get_db_url():
     try:
+        # Tenta pegar dos Secrets
         return st.secrets["DATABASE_URL"]
     except (FileNotFoundError, KeyError):
-        load_dotenv()
         return os.getenv("DATABASE_URL")
 
 DB_URL = get_db_url()
